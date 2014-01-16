adb0facebe866f4ecefe28430fda694b1f59eb4e
ataylor <github@rxtaylor.com>
2014-01-16 15:26:10 -0500

    Start

--- CallReport/index.php
+++ CallReport/index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="rsc/pure-min.css" />
+        <link rel="stylesheet" href="rsc/pureskin.css" type="text/css" />
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <link rel="shortcut icon" href="rsc/favicon.ico" />
         <title></title>
     </head>
     <body>
